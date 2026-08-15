7d8e9780ee09ce7aaa3a4efc5de9ea15927e36f3
kritikaa1302 <kritikagrover1302@gmail.com>
2026-08-15 22:19:28 +0530

    Update Section2.jsx

--- 07-ui-project/src/components/Section2/Section2.jsx
+++ 07-ui-project/src/components/Section2/Section2.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Section2 = () => {
+const Section2 = (props) => {
   return (
-    <div className='h-screen w-full bg-gray-900'>Section2</div>
+    <div className='h-screen w-full bg-gray-900'>
+         <Navbar />
+        <Page1Content users={props.users} /></div>
   )
 }
+
+
+
 
 export default Section2
