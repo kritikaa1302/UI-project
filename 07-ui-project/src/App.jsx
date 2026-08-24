60db3d3b7375517f1496f5fb7e733be7145b7daa
kritikaa1302 <kritikagrover1302@gmail.com>
2026-08-24 23:53:47 +0530

    Update App.jsx

--- 07-ui-project/src/App.jsx
+++ 07-ui-project/src/App.jsx
@@ -2,7 +2,6 @@ import Section1 from './components/Section1/Section1'
 import Section2 from './components/Section2/Section2'
 
 const App = () => {
-  
   const users = [
     {
       img: 'https://images.unsplash.com/photo-1573496359142-b8d87734a5a2?q=80&w=2576&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
